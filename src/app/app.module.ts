9dc98937545e2149e32eeacbfd2a768a76cf2aeb
DhiaTr <trabelsidhia19@gmail.com>
2021-10-02 18:52:03 +0100

    projects section structure set and partial style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { ProjectsSectionComponent } from './sections/projects-section/projects-s
 import { ContactSectionComponent } from './sections/contact-section/contact-section.component';
 import { OtherLinksSectionComponent } from './sections/other-links-section/other-links-section.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { AnimatedButtonComponent } from './shared-components/animated-button/animated-button.component';
+import { AnimatedButtonComponent } from './components/animated-button/animated-button.component';
+import { ProjectTemplateComponent } from './components/project-template/project-template.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { AnimatedButtonComponent } from './shared-components/animated-button/ani
     ProjectsSectionComponent,
     ContactSectionComponent,
     OtherLinksSectionComponent,
-    AnimatedButtonComponent
+    AnimatedButtonComponent,
+    ProjectTemplateComponent
   ],
   imports: [
     BrowserModule,
