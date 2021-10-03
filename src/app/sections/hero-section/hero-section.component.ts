107d95c71941b668c9d9e269725933ca2cccaa33
DhiaTr <trabelsidhia19@gmail.com>
2021-10-03 11:43:17 +0100

    animation reusage and hero section animation clean

--- src/app/sections/hero-section/hero-section.component.ts
+++ src/app/sections/hero-section/hero-section.component.ts
@@ -1,37 +1,26 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {animate, animation, state, style, transition, trigger, useAnimation} from "@angular/animations";
 import { AnimationEvent } from "@angular/animations";
+import { slideIn } from '../../components/animations';
 
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
   styleUrls: ['./hero-section.component.scss', 'hero-section.component.query.scss'],
-  animations: [
-    trigger('heroAnimation', [
-      state("In", style({
-        'opacity': '1',
-        'transform': 'translateX(0)'
-      })),
-      state("standBy", style({
-        'opacity': '0',
-        'transform': 'translateX(-5%)'
-      })),
-      transition('standBy => In', animate("900ms 0ms cubic-bezier(0.5, 0, 0, 1)"))
-    ])
-  ]
+  animations: [ slideIn ]
 })
 export class HeroSectionComponent implements AfterViewInit {
 
-  titleState = 'standBy';
-  buttonState = 'standBy';
+  titleState = 'standByRight';
+  buttonState = 'standByRight';
   constructor() { }
 
   animationDone(event: AnimationEvent) {
     if(event.totalTime)
-      this.buttonState = 'In';
+      this.buttonState = 'inFromRight';
   }
 
   ngAfterViewInit(): void {
-    this.titleState="In";
+    this.titleState="inFromRight";
   }
 }
