5439e3bce9d857c2f47119f02ef9057e34530137
DhiaTr <trabelsidhia19@gmail.com>
2021-10-04 11:24:06 +0100

    contact section animations

--- src/app/sections/contact-section/contact-section.component.ts
+++ src/app/sections/contact-section/contact-section.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import { appear, appearIn } from '../../components/animations';
 
 @Component({
   selector: 'app-contact-section',
   templateUrl: './contact-section.component.html',
-  styleUrls: ['./contact-section.component.scss']
+  styleUrls: ['./contact-section.component.scss'],
+  animations: [ appear, appearIn ]
 })
 export class ContactSectionComponent implements OnInit {
 
+  titleState = 'standBy';
+  infoState = 'standByBottom';
+  triggered = false;
   constructor() { }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any): void {
+    if (window.scrollY  > 2500 && !this.triggered) {
+      this.titleState = 'In';
+      this.infoState = 'inFromBottom';
+    }
+  }
+
   ngOnInit(): void {
+
   }
 
 }
