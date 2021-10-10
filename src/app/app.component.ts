d67868823ecbafaa06f157cd6da0709da606a809
DhiaTr <trabelsidhia19@gmail.com>
2021-10-10 14:58:20 +0100

    rebuild & title set from data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { style } from './data';
+import { style, personalData } from './data';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,13 @@ export class AppComponent implements OnInit{
     { name: 'main-color', value: style.gradiantStartColor },
     { name: 'second-color', value: style.gradiantEndColor },
   ];
+  constructor(private titleService: Title) {}
 
   ngOnInit(): void {
     this.styles.forEach(data => {
       document.documentElement.style.setProperty(`--${data.name}`, data.value);
     });
+      this.titleService.setTitle(personalData.fullName + ' | Portfolio');
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, Title} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +24,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
